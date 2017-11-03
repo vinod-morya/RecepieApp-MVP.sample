28c5e499669d990bf59f93795c6922036421e684
Vinod Morya <vinod.morya@wildnettechnologies.com>
2017-11-03 13:51:08 +0530

    MPV simple

--- app/src/main/java/com/wildnettechnologies/recepieonmvp/Recepie/RecepieActivity.java
+++ app/src/main/java/com/wildnettechnologies/recepieonmvp/Recepie/RecepieActivity.java
@@ -34,159 +34,178 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+public class RecepieActivity extends AppCompatActivity implements IRecepieView
+  {
 
-public class RecepieActivity extends AppCompatActivity implements IRecepieView {
+	 @BindView(R.id.tvWelcomeText)
+	 TextView mTvWelcomeText;
+	 @BindView(R.id.recyclerViewRecepie)
+	 RecyclerView mRecyclerViewRecepie;
+	 @BindView(R.id.progressbarRecepie)
+	 ProgressBar mProgressbarRecepie;
+	 private RecepieAdapter mRecepieAdapter;
+	 private ArrayList<RecepieModel.Result> mRecepieModel = new ArrayList<>();
+	 private SearchView mSearchView;
+	 private RecepiePresenter mRecepiePresenter;
+	 private RecepieRequestModel mRecepieRequestModel;
 
+	 @Override
+	 protected void onCreate(Bundle savedInstanceState)
+		{
+		  super.onCreate(savedInstanceState);
+		  setContentView(R.layout.activity_recepie);
+		  ButterKnife.bind(this);
+		  initializeViews();
+		}
 
-    @BindView(R.id.tvWelcomeText)
-    TextView mTvWelcomeText;
-    @BindView(R.id.recyclerViewRecepie)
-    RecyclerView mRecyclerViewRecepie;
-    @BindView(R.id.progressbarRecepie)
-    ProgressBar mProgressbarRecepie;
-    private RecepieAdapter mRecepieAdapter;
-    private ArrayList<RecepieModel.Result> mRecepieModel = new ArrayList<>();
-    private SearchView mSearchView;
-    private RecepiePresenter mRecepiePresenter;
-    private RecepieRequestModel mRecepieRequestModel;
+	 private void initializeViews()
+		{
+		  RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+		  mRecyclerViewRecepie.setLayoutManager(layoutManager);
+		  mRecepieAdapter = new RecepieAdapter(this, mRecepieModel);
+		  mRecyclerViewRecepie.setAdapter(mRecepieAdapter);
+		  mRecepiePresenter = new RecepiePresenterImpl(this);
+		}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recepie);
-        ButterKnife.bind(this);
-        initializeViews();
-    }
+	 @Override
+	 public boolean onCreateOptionsMenu(Menu menu)
+		{
+		  super.onCreateOptionsMenu(menu);
+		  MenuInflater inflater = getMenuInflater();
+		  inflater.inflate(R.menu.menu_search, menu);
+		  mSearchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+		  SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		  mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+		  initSearch();
+		  return true;
+		}
 
-    private void initializeViews() {
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        mRecyclerViewRecepie.setLayoutManager(layoutManager);
-        mRecepieAdapter = new RecepieAdapter(this, mRecepieModel);
-        mRecyclerViewRecepie.setAdapter(mRecepieAdapter);
-        mRecepiePresenter = new RecepiePresenterImpl(this);
+	 @Override
+	 public boolean onOptionsItemSelected(MenuItem item)
+		{
+		  switch(item.getItemId())
+		  {
+			 case R.id.action_search:
+				return true;
+			 default:
+				return super.onOptionsItemSelected(item);
+		  }
+		}
 
-    }
+	 @Override
+	 public void showProgress()
+		{
+		  runOnUiThread(new Runnable()
+			 {
+				@Override
+				public void run()
+				  {
+					 mProgressbarRecepie.setVisibility(View.VISIBLE);
+					 mRecyclerViewRecepie.setVisibility(View.INVISIBLE);
+				  }
+			 });
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
-        mSearchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        initSearch();
-        return true;
-    }
+	 @Override
+	 public void hideProgress()
+		{
+		  mProgressbarRecepie.setVisibility(View.INVISIBLE);
+		  mRecyclerViewRecepie.setVisibility(View.VISIBLE);
+		}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_search:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+	 @Override
+	 public void hideDefaultText()
+		{
+		  mTvWelcomeText.setVisibility(View.INVISIBLE);
+		}
 
+	 @Override
+	 public void setItems(List<RecepieModel.Result> items, int pageNumber)
+		{
+		  if(pageNumber == AppConstants.DEFAULT_PAGE)
+		  {
+			 mRecepieModel.clear();
+		  }
+		  mRecepieModel.addAll(items);
+		  mRecepieAdapter.notifyDataSetChanged();
+		}
 
-    @Override
-    public void showProgress() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mProgressbarRecepie.setVisibility(View.VISIBLE);
-                mRecyclerViewRecepie.setVisibility(View.INVISIBLE);
-            }
-        });
-    }
+	 private void initSearch()
+		{
+		  if(mSearchView != null)
+		  {
+			 mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+				{
 
-    @Override
-    public void hideProgress() {
-        mProgressbarRecepie.setVisibility(View.INVISIBLE);
-        mRecyclerViewRecepie.setVisibility(View.VISIBLE);
-    }
+				  Timer timer = new Timer();
+				  final long DELAY = 1000; // milliseconds
 
-    @Override
-    public void hideDefaultText() {
-        mTvWelcomeText.setVisibility(View.INVISIBLE);
-    }
+				  @Override
+				  public boolean onQueryTextSubmit(String query)
+					 {
+						if(query.length() > 0)
+						{
+						  mRecepieRequestModel = new RecepieRequestModel();
+						  mRecepieRequestModel.setRecepie(query);
+						  mRecepieRequestModel.setPage(1);
+						  mRecepiePresenter.onSearchStarted(mRecepieRequestModel);
+						}
+						return false;
+					 }
 
-    @Override
-    public void setItems(List<RecepieModel.Result> items, int pageNumber) {
-        if (pageNumber == AppConstants.DEFAULT_PAGE) {
-            mRecepieModel.clear();
-        }
-        mRecepieModel.addAll(items);
-    }
+				  @Override
+				  public boolean onQueryTextChange(final String newText)
+					 {
+						if(newText.length() > 0)
+						{
+						  Log.e("Search text", "Search Text : " + newText);
+						  timer.cancel();
+						  timer = new Timer();
+						  timer.schedule(new TimerTask()
+							 {
+								@Override
+								public void run()
+								  {
+									 mRecepieRequestModel = new RecepieRequestModel();
+									 mRecepieRequestModel.setRecepie(newText);
+									 mRecepieRequestModel.setPage(1);
+									 mRecepiePresenter.onSearchStarted(mRecepieRequestModel);
+								  }
+							 }, DELAY);
+						}
+						return false;
+					 }
+				});
 
-    private void initSearch() {
-        if (mSearchView != null) {
-            mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+			 mSearchView.setOnCloseListener(new SearchView.OnCloseListener()
+				{
+				  @Override
+				  public boolean onClose()
+					 {
+						return false;
+					 }
+				});
+		  }
+		}
 
-                Timer timer = new Timer();
-                final long DELAY = 1000; // milliseconds
+	 @OnClick(R.id.tvWelcomeText)
+	 public void onViewClicked()
+		{
 
+		}
 
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    if (query.length() > 0) {
-                        mRecepieRequestModel = new RecepieRequestModel();
-                        mRecepieRequestModel.setRecepie(query);
-                        mRecepieRequestModel.setPage(1);
-                        mRecepiePresenter.onSearchStarted(mRecepieRequestModel);
-                    }
-                    return false;
-                }
-
-                @Override
-                public boolean onQueryTextChange(final String newText) {
-                    if (newText.length() > 0) {
-                        Log.e("Search text", "Search Text : " + newText);
-                        timer.cancel();
-                        timer = new Timer();
-                        timer.schedule(
-                                new TimerTask() {
-                                    @Override
-                                    public void run() {
-                                        mRecepieRequestModel = new RecepieRequestModel();
-                                        mRecepieRequestModel.setRecepie(newText);
-                                        mRecepieRequestModel.setPage(1);
-                                        mRecepiePresenter.onSearchStarted(mRecepieRequestModel);
-                                    }
-                                },
-                                DELAY
-                        );
-
-                    }
-                    return false;
-                }
-            });
-
-            mSearchView.setOnCloseListener(new SearchView.OnCloseListener() {
-                @Override
-                public boolean onClose() {
-                    return false;
-                }
-            });
-        }
-    }
-
-    @OnClick(R.id.tvWelcomeText)
-    public void onViewClicked() {
-
-    }
-
-    @OnClick({R.id.tvWelcomeText, R.id.progressbarRecepie})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.tvWelcomeText:
-                Log.e("welcome Text Clicked", "Welcome clicked");
-                break;
-            case R.id.recyclerViewRecepie:
-                break;
-            case R.id.progressbarRecepie:
-                break;
-        }
-    }
-}
+	 @OnClick({R.id.tvWelcomeText, R.id.progressbarRecepie})
+	 public void onViewClicked(View view)
+		{
+		  switch(view.getId())
+		  {
+			 case R.id.tvWelcomeText:
+				Log.e("welcome Text Clicked", "Welcome clicked");
+				break;
+			 case R.id.recyclerViewRecepie:
+				break;
+			 case R.id.progressbarRecepie:
+				break;
+		  }
+		}
+  }
